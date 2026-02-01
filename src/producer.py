c92aed154c09d1cef894fda92e80131aaf397dcf
mMexSugar <tsiganoktv04@gmail.com>
2026-02-02 00:58:25 +0200

    add schema table in bg and pubsub_bq_writer

--- src/producer.py
+++ src/producer.py
@@ -1,30 +1,61 @@
 import json
 import requests
+import time
+from datetime import datetime, timedelta
 from google.cloud import pubsub_v1
+import os
 
+PROJECT_ID = "sublime-wavelet-485112-m9"
+TOPIC_ID = "openaq-data-topic"
+CREDENTIALS_PATH = os.path.join("terraform", "keys.json")
 
-project_id = "sublime-wavelet-485112-m9"
-topic_id = "openaq-data-topic"
+publisher = pubsub_v1.PublisherClient.from_service_account_json(CREDENTIALS_PATH)
+topic_path = publisher.topic_path(PROJECT_ID, TOPIC_ID)
 
-credentials_path = "terraform/keys.json"
-
-publisher = pubsub_v1.PublisherClient.from_service_account_json(credentials_path)
-topic_path = publisher.topic_path(project_id, topic_id)
+def transform_data(record):
+    """Приводит данные OpenAQ к нашей схеме BigQuery"""
+    coordinates = record.get('coordinates', {}) or {}
+    return {
+        "location_id": record.get('locationId'),
+        "location": record.get('location'),
+        "city": record.get('city'),
+        "country": record.get('country'),
+        "parameter": record.get('parameter'),
+        "value": record.get('value'),
+        "unit": record.get('unit'),
+        "timestamp": record.get('date', {}).get('utc'),
+        "latitude": coordinates.get('latitude'),
+        "longitude": coordinates.get('longitude')
+    }
 
 def fetch_and_publish():
-    url = "https://api.openaq.org/v2/measurements?limit=10&country=UA"
-    response = requests.get(url)
+    #  записи за последние 2 минуты создает overlap, чтобы не потерять данные из-за задержек API
+    time_threshold = (datetime - timedelta(minutes=2)).isoformat()
     
-    if response.status_code == 200:
-        data = response.json()['results']
+    url = f"https://api.openaq.org/v2/measurements?limit=100&date_from={time_threshold}&order_by=datetime"
+    
+    try:
+        print(f"[{datetime.now()}] Запрос новых данных (после {time_threshold})...")
+        response = requests.get(url, timeout=15)
+        response.raise_for_status()
         
-        for record in data:
-            message_data = json.dumps(record).encode("utf-8")
-            
-            future = publisher.publish(topic_path, message_data)
-            print(f"Опубликовано сообщение: {future.result()}")
-    else:
-        print(f"Ошибка API: {response.status_code}")
+        results = response.json().get('results', [])
+        print(f"Получено {len(results)} новых записей.")
+
+        for record in results:
+            clean_data = transform_data(record)
+            if clean_data["timestamp"]:
+                message_bytes = json.dumps(clean_data).encode("utf-8")
+                publisher.publish(topic_path, message_bytes)
+        
+        print("Данные отправлены в Pub/Sub.")
+
+    except Exception as e:
+        print(f"Ошибка при сборе данных: {e}")
 
 if __name__ == "__main__":
-    fetch_and_publish()
+    print("Запуск мониторинга качества воздуха (Global)...")
+    while True:
+        fetch_and_publish()
+        print("Ожидание 60 секунд...")
+        time.sleep(60)
--- terraform/main.tf
+++ terraform/main.tf
@@ -41,3 +41,60 @@ output "pubsub_topic_name" {
 output "bigquery_dataset_id" {
   value = google_bigquery_dataset.openaq_dataset.dataset_id
 }
+
+resource "google_bigquery_table" "air_quality_table" {
+  dataset_id = google_bigquery_dataset.openaq_dataset.dataset_id
+  table_id   = "measurements_raw"
+
+  schema = <<EOF
+[
+  {"name": "location_id", "type": "INTEGER", "mode": "NULLABLE"},
+  {"name": "location", "type": "STRING", "mode": "NULLABLE"},
+  {"name": "city", "type": "STRING", "mode": "NULLABLE"},
+  {"name": "country", "type": "STRING", "mode": "NULLABLE"},
+  {"name": "parameter", "type": "STRING", "mode": "NULLABLE"},
+  {"name": "value", "type": "FLOAT", "mode": "NULLABLE"},
+  {"name": "unit", "type": "STRING", "mode": "NULLABLE"},
+  {"name": "timestamp", "type": "TIMESTAMP", "mode": "REQUIRED"},
+  {"name": "latitude", "type": "FLOAT", "mode": "NULLABLE"},
+  {"name": "longitude", "type": "FLOAT", "mode": "NULLABLE"}
+]
+EOF
+
+  time_partitioning {
+    type  = "DAY"
+    field = "timestamp"
+  }
+
+  clustering = ["country", "city"]
+}
+
+#  Pub/Sub
+data "google_project" "project" {}
+
+
+resource "google_project_iam_member" "pubsub_bq_writer" {
+  project = data.google_project.project.project_id
+  role    = "roles/bigquery.dataEditor"
+  member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "pubsub_bq_metadata" {
+  project = data.google_project.project.project_id
+  role    = "roles/bigquery.metadataViewer"
+  member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+}
+
+resource "google_pubsub_subscription" "bq_subscription" {
+  name  = "openaq-bq-sub"
+  topic = google_pubsub_topic.openaq_topic.name
+
+  bigquery_config {
+    table = "${data.google_project.project.project_id}.${google_bigquery_dataset.openaq_dataset.dataset_id}.${google_bigquery_table.air_quality_table.table_id}"
+    
+    use_topic_schema = false 
+    write_metadata   = false
+  }
+
+  depends_on = [google_project_iam_member.pubsub_bq_writer]
+}
